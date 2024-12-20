adde5d0d8b52991455c2b434c9af2512661a14ba
Victor Angelo <51969107+Victor476@users.noreply.github.com>
2024-12-20 20:54:40 -0300

    atividade 2 inicio copia 1

---
+++ Atividade02_LesMiserables/src/Main.java
@@ -0,0 +1,113 @@
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import lib.BreadthFirstPaths;
+import lib.Graph;
+import lib.StdOut;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Graph graph = readGraphFromGexf();
+
+        System.out.println("Les Miserables Graph: ");
+        StdOut.println(graph);
+
+        int[] eccentricities = calculateEccentricity(graph);
+        System.out.println("Excentricidades: ");
+        for (int i = 0; i < eccentricities.length; i++) {
+            StdOut.println("Excentricidade do vértice " + i + ": " + eccentricities[i]);
+        }
+        System.out.println();
+
+        double[] ccs = calculateClosenessCentrality(graph);
+        System.out.println("Closeness Centralities: ");
+        for (int i = 0; i < eccentricities.length; i++) {
+            StdOut.printf("CC do vértice %d: %.6f\n", i, ccs[i]);
+        }
+        System.out.println();
+    }
+
+    
+    private static Graph readGraphFromGexf() throws Exception {
+
+        final String GEXF_FILE = System.getProperty("user.dir") + "/src/data/LesMiserables.gexf";
+
+        // Carrega o arquivo .gexf
+        File file = new File(GEXF_FILE);
+
+        // Cria um DocumentBuilder
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // Converte o .gexf
+        Document gexfDoc = builder.parse(file);
+
+        NodeList nodes = gexfDoc.getElementsByTagName("node");
+        int nodesCount = nodes.getLength();
+
+        NodeList edges = gexfDoc.getElementsByTagName("edge");
+        int edgesCount = edges.getLength();
+
+        Graph graph = new Graph(nodesCount);
+
+        for (int i = 0; i < edgesCount; i++) {
+            Node edge = edges.item(i);
+
+            if (edge.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) edge;
+                Integer source = (int) Float.parseFloat(element.getAttribute("source"));
+                Integer target = (int) Float.parseFloat(element.getAttribute("target"));
+
+                graph.addEdge(source, target);                
+            }
+        }
+
+        return graph;
+    }
+
+    private static int[] calculateEccentricity(Graph graph) {
+        // 1. Roda a busca em largura
+        // 2. Obtém todas as distâncias mínimas a partir do vértice fonte para os outros vértices
+        // 3. Obtém a maior distância mínima (excentricidade)
+
+        int verticesCount = graph.V();
+
+        int eccentricities[] = new int[verticesCount];
+        for (int v = 0; v < verticesCount; v++) {
+            BreadthFirstPaths bfp = new BreadthFirstPaths(graph, v);
+
+            int eccentricity = bfp.greatestDist();
+
+            eccentricities[v] = eccentricity;
+        }
+
+        return eccentricities;
+    }
+
+    private static double[] calculateClosenessCentrality(Graph graph) {
+        // 1. Calcular: total de vértices -1 / soma de todas as distâncias mínimas a partir do vértice fonte para os outros vértices
+
+
+        int verticesCount = graph.V();
+
+        double values[] = new double[verticesCount];
+        for (int v = 0; v < verticesCount; v++) {
+            BreadthFirstPaths bfp = new BreadthFirstPaths(graph, v);
+
+            double distToSum = (double) bfp.sumDistTo();
+            double closenessCentrality = (verticesCount - 1) / distToSum;
+
+            values[v] = closenessCentrality;
+        }
+
+        return values;
+    }
+}
